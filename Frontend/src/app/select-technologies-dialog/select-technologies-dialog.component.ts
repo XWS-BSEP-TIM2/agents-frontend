695dc3d277955b055d35c1c9377c2f42190bb36b
Aleksandar Savic <savic.ra8.2018@uns.ac.rs>
2022-06-17 23:52:19 +0200

    feat: fix vulnerabilities

--- Frontend/src/app/select-technologies-dialog/select-technologies-dialog.component.ts
+++ Frontend/src/app/select-technologies-dialog/select-technologies-dialog.component.ts
@@ -1,4 +1,3 @@
-import { ResolvedStaticSymbol } from '@angular/compiler';
 import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
